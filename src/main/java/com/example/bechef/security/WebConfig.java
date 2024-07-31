945b2525342dcf92e658926e7823bbaf37389604
앙귀머랭 <osr3173@gmail.com>
2024-07-31 14:34:18 +0900

    deploy

--- src/main/java/com/example/bechef/security/WebConfig.java
+++ src/main/java/com/example/bechef/security/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")    // 모든 주소 적용됨
 //                .allowedOrigins("Http://localhost:3000") // 테스트용 도메인
                 .allowedOrigins("https://mango-sand-0aec9c100.5.azurestaticapps.net") // 배포용 도메인
-                .allowedMethods("GET", "POST", ""PUT", "DELETE","OPTIONS") // HTTP request 메서드 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // HTTP request 메서드 허용
                 .allowedHeaders("*") // request의 모든 header 허용
                 .exposedHeaders("Authorization") // 클라이언트가 authorization은 볼수있게 해줌
                 .allowCredentials(true); // 클라이언트 관련 메소드
